f29314a114bc714c0b9798b3a862c015c7c9bc2d
Sadra Jalili <sadra.jl79@gmail.com>
2024-04-06 00:06:59 +0330

    removed history widget as I already have a fish plugin for that

--- .config/fish/conf.d/fzf_keys.fish
+++ .config/fish/conf.d/fzf_keys.fish
@@ -47,27 +47,27 @@ function fzf_key_bindings
     commandline -f repaint
   end
 
-  function fzf-history-widget -d "Show command history"
-    test -n "$FZF_TMUX_HEIGHT"; or set FZF_TMUX_HEIGHT 40%
-    begin
-      set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT $FZF_DEFAULT_OPTS --scheme=history --bind=ctrl-r:toggle-sort,ctrl-z:ignore $FZF_CTRL_R_OPTS +m"
+  # function fzf-history-widget -d "Show command history"
+  #   test -n "$FZF_TMUX_HEIGHT"; or set FZF_TMUX_HEIGHT 40%
+  #   begin
+  #     set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT $FZF_DEFAULT_OPTS --scheme=history --bind=ctrl-r:toggle-sort,ctrl-z:ignore $FZF_CTRL_R_OPTS +m"
 
-      set -l FISH_MAJOR (echo $version | cut -f1 -d.)
-      set -l FISH_MINOR (echo $version | cut -f2 -d.)
+  #     set -l FISH_MAJOR (echo $version | cut -f1 -d.)
+  #     set -l FISH_MINOR (echo $version | cut -f2 -d.)
 
-      # history's -z flag is needed for multi-line support.
-      # history's -z flag was added in fish 2.4.0, so don't use it for versions
-      # before 2.4.0.
-      if [ "$FISH_MAJOR" -gt 2 -o \( "$FISH_MAJOR" -eq 2 -a "$FISH_MINOR" -ge 4 \) ];
-        history -z | eval (__fzfcmd) --read0 --print0 -q '(commandline)' | read -lz result
-        and commandline -- $result
-      else
-        history | eval (__fzfcmd) -q '(commandline)' | read -l result
-        and commandline -- $result
-      end
-    end
-    commandline -f repaint
-  end
+  #     # history's -z flag is needed for multi-line support.
+  #     # history's -z flag was added in fish 2.4.0, so don't use it for versions
+  #     # before 2.4.0.
+  #     if [ "$FISH_MAJOR" -gt 2 -o \( "$FISH_MAJOR" -eq 2 -a "$FISH_MINOR" -ge 4 \) ];
+  #       history -z | eval (__fzfcmd) --read0 --print0 -q '(commandline)' | read -lz result
+  #       and commandline -- $result
+  #     else
+  #       history | eval (__fzfcmd) -q '(commandline)' | read -l result
+  #       and commandline -- $result
+  #     end
+  #   end
+  #   commandline -f repaint
+  # end
 
   function fzf-cd-widget -d "Change directory"
     set -l commandline (__fzf_parse_commandline)

a9eb90fc44ee7a73b2dbdd558ba771c8799354bd
Sadra Jalili <sadra.jl79@gmail.com>
2024-04-06 14:57:27 +0330

    refactored and added key maps

--- .config/lvim/lua/pale_lotus/options.lua
+++ .config/lvim/lua/pale_lotus/options.lua
@@ -1,13 +1,25 @@
+--keymaps
+local function map(mode, key, value, description)
+    if description == nil then
+        vim.keymap.set(mode, key, value, { silent = true })
+        return
+    end
+        vim.keymap.set(mode, key, value, { desc = description })
+end
+
+--changed open buffer(tabs) with tab and shift-tab
+map('n', '<Tab>', vim.cmd.bnext,   'Go to next buffer')
+map('n', '<S-Tab>', vim.cmd.bprev, 'Go to previous buffer')
+
+-- Mimic shell movements
+map("i", "<C-E>", "<ESC>A")
+
+--changed open buffer(tabs) with gt and gT
+lvim.keys.normal_mode["gt"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["gT"] = ":BufferLineCyclePrev<CR>"
+
 vim.opt.shiftwidth = 4
 
 --help with jumping.
 --vim.opt.relativenumber = true
-
-
-vim.keymap.set('n', '<Tab>', vim.cmd.bnext, { desc = 'Go to next buffer' })
-vim.keymap.set('n', '<S-Tab>', vim.cmd.bprev, { desc = 'Go to previous buffer' })
-
-
-lvim.keys.normal_mode["gt"] = ":BufferLineCycleNext<CR>"
-lvim.keys.normal_mode["gT"] = ":BufferLineCyclePrev<CR>"
 

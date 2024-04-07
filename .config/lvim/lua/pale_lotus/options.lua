2637adc2a4a59e3dc283b5f0f4a3107f34e59246
Sadra Jalili <sadra.jl79@gmail.com>
2024-04-07 16:02:35 +0330

    changed vim shell to bash (as its taking long time like 1 ~ 1.5 secends)

--- .config/lvim/lua/pale_lotus/options.lua
+++ .config/lvim/lua/pale_lotus/options.lua
@@ -13,7 +13,7 @@ map('n', '<S-Tab>', vim.cmd.bprev, 'Go to previous buffer')
 
 -- Mimic shell movements
 map("i", "<C-E>", "<ESC>A")
-
+map("i", "<C-A>", "<ESC>I")
 --changed open buffer(tabs) with gt and gT
 lvim.keys.normal_mode["gt"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["gT"] = ":BufferLineCyclePrev<CR>"
@@ -30,3 +30,10 @@ for name, value in pairs(opts) do
   vim.opt[name] = value
 end
 
+-- If the current system shell or the `shell` option is set to /usr/bin/fish then revert to sh
+if os.getenv('SHELL') == "/usr/bin/fish" or vim.opt.shell == "/usr/bin/fish" then
+    vim.opt.shell = "/bin/bash"
+else
+   -- Else default to the system current shell.
+   vim.opt.shell = os.getenv('SHELL')
+end

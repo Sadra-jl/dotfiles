e77ebd160f17375ea2b760823ccd4de03e1ced75
Sadra Jalili <sadra.jl79@gmail.com>
2024-04-24 12:00:30 +0330

    require source files dynamically

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,7 +1,22 @@
-reload("pale_lotus.plugins.python")
-reload("pale_lotus.plugins.hyprland")
-reload("pale_lotus.options")
+local info = debug.getinfo(1, 'S')
+local module_directory = string.match(info.source, '^@?(.*)/') or "."
+local module_filename = string.match(info.source, '/([^/]*)$')
 
-reload("lsp.python")
-reload("lsp.bash")
+function Scandir(directory)
+     local i, t, popen = 0, {}, io.popen
+     local pfile = popen('find "' .. directory .. '/lua" -type f -name "*.lua" | sed "s/.*lua\\///; s/\\//./g; s/.lua//; s/' .. 'module_filename//"')
 
+     if pfile == nil then return {} end
+
+     for filename in pfile:lines() do
+         i = i + 1
+         t[i] = filename
+     end
+
+     pfile:close()
+     return t
+ end
+
+ for _, filename in ipairs(Scandir(module_directory)) do
+  require(filename)
+ end

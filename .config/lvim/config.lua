f7d38e2fb3a8bd1934997c5a7b841edeec7fcf54
Sadra Jalili <sadra.jl79@gmail.com>
2024-04-24 12:15:00 +0330

    used reload instead of require

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -3,20 +3,21 @@ local module_directory = string.match(info.source, '^@?(.*)/') or "."
 local module_filename = string.match(info.source, '/([^/]*)$')
 
 function Scandir(directory)
-     local i, t, popen = 0, {}, io.popen
-     local pfile = popen('find "' .. directory .. '/lua" -type f -name "*.lua" | sed "s/.*lua\\///; s/\\//./g; s/.lua//; s/' .. 'module_filename//"')
+    local i, t, popen = 0, {}, io.popen
+    local pfile = popen('find "' ..
+    directory .. '/lua" -type f -name "*.lua" | sed "s/.*lua\\///; s/\\//./g; s/.lua//; s/' .. module_filename .. '//"')
 
-     if pfile == nil then return {} end
+    if pfile == nil then return {} end
 
-     for filename in pfile:lines() do
-         i = i + 1
-         t[i] = filename
-     end
+    for filename in pfile:lines() do
+        i = i + 1
+        t[i] = filename
+    end
 
-     pfile:close()
-     return t
- end
+    pfile:close()
+    return t
+end
 
- for _, filename in ipairs(Scandir(module_directory)) do
-  require(filename)
- end
+for _, filename in ipairs(Scandir(module_directory)) do
+    reload(filename)
+end
